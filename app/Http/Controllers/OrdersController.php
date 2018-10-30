cdcf67ab10e6517f3db4ef1f767bbe2ffccd0d5a
vagrant <vagrant@homestead>
2018-10-30 14:48:23 +0000

    用户界面 - 订单详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -13,6 +13,18 @@ use App\Jobs\CloseOrder;
 
 class OrdersController extends Controller
 {
+  public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
+    }
+
   public function store(OrderRequest $request)
   {
       $user  = $request->user();
@@ -69,4 +81,10 @@ class OrdersController extends Controller
 
       return $order;
   }
+
+  public function show(Order $order, Request $request)
+    {
+        $this->authorize('own', $order);
+        return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
 }
